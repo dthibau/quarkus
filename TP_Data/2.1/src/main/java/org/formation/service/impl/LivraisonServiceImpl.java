a8efe1581d4bdc5ef514e6537a1210551fde11a9
David THIBAU <david.thibau@gmail.com>
2022-05-05 09:08:12 +0200

    Sources TP2

--- TP_Data/2.1/src/main/java/org/formation/service/impl/LivraisonServiceImpl.java
+++ TP_Data/2.1/src/main/java/org/formation/service/impl/LivraisonServiceImpl.java
@@ -3,16 +3,35 @@ package org.formation.service.impl;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.formation.config.NotificationServiceConfig;
 import org.formation.domain.Livraison;
 import org.formation.domain.Livreur;
 import org.formation.domain.Status;
+import org.formation.interceptor.Logged;
 import org.formation.service.LivraisonService;
 
+import io.smallrye.mutiny.Multi;
+
+@ApplicationScoped
+@Logged
 public class LivraisonServiceImpl implements LivraisonService {
 
+	@Inject
+	NotificationServiceConfig notificationServiceConfig;
+	
 	List<Livraison> livraisons;
 	
+	@ConfigProperty(name = "quarkus.http.port") 
+	String port;
+	
+	@PostConstruct
 	public void init() {
 		livraisons = new ArrayList<>();
 		livraisons.add(Livraison.builder().id(1).noCommande("1").creationDate(Instant.now()).status(Status.DISTRIBUE).build());
@@ -21,14 +40,22 @@ public class LivraisonServiceImpl implements LivraisonService {
 		livraisons.add(Livraison.builder().id(1).noCommande("4").creationDate(Instant.now()).status(Status.DISTRIBUE).build());		
 	}
 	@Override
-	public List<Livraison> findAll() {
-		return livraisons;
+	public Multi<Livraison> findAll() {
+		System.out.println("Notificaiton Service Config " + notificationServiceConfig);
+		return Multi.createFrom().items(livraisons.stream()); 
 	}
 
 	@Override
-	public void create(String noCommande) {
-		livraisons.add(Livraison.builder().id(livraisons.size()+1).noCommande(noCommande).creationDate(Instant.now()).status(Status.CREE).build());
+	public Optional<Livraison> load(Livraison livraison) {
+		int index = livraisons.indexOf(livraison);
+		return index == -1 ? Optional.empty() : Optional.of(livraisons.get(index));
+	}
 		
+	@Override
+	public Livraison create(String noCommande) {
+		Livraison livraison = Livraison.builder().id(livraisons.size()+1).noCommande(noCommande).creationDate(Instant.now()).status(Status.CREE).build();
+		livraisons.add(livraison);
+		return livraison;
 	}
 
 	@Override
